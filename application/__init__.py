0865c5f386caa5d2a7802619802d3fff6688860c
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-12-11 17:11:05 -0300

    fix: use env variable instead dot env

--- application/__init__.py
+++ application/__init__.py
@@ -1,25 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
-from dotenv import load_dotenv
-from os import environ
 
 import sys
 import os
 
 sys.path.append(os.getcwd())
 
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
-app.config["MONGO_URI"] = environ.get("MONGO_URI")
-app.config["MAIL_SERVER"] = environ.get("MAIL_SERVER")
-app.config["MAIL_PORT"] = environ.get("MAIL_PORT")
-app.config["MAIL_USERNAME"] = environ.get("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = environ.get("MAIL_PASSWORD")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.getenv("MAIL_PORT")
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 
