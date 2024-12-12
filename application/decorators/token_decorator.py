d52b0fd5b34b3ce22c5e1cbd2b4ed1048b6e2929
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-12-12 02:12:44 -0300

    fix: token_required function

--- application/decorators/token_decorator.py
+++ application/decorators/token_decorator.py
@@ -1,21 +1,28 @@
-import jwt
 from functools import wraps
 from flask import request, jsonify
-from application import app, db
+from application import app
+from bson import ObjectId
+import jwt
 
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get("Authorization")
-        if not token:
-            return jsonify({"message": "Token is missing!", "status": 401}), 401
+        auth_header = request.headers.get("Authorization")
+        if not auth_header or "Bearer " not in auth_header:
+            return jsonify({"message": "Missing token", "status": 400}), 400
+
+        token = auth_header.split(" ")[1]
 
         try:
-            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-            request.user_id = data["user_id"]
+            decoded = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+            request.user_id = ObjectId(decoded["user_id"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message": "Expired token", "status": 400}), 400
         except Exception as e:
-            return jsonify({"message": "Invalid token", "status": 401}), 401
+            print(e)
+            return jsonify({"message": "Invalid token", "status": 400}), 400
+
         return f(*args, **kwargs)
 
     return decorated
